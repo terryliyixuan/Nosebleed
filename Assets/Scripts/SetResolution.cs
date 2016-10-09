c745f5dafeb99c795e6fbb1a1cd7f4040490f6de
Terry Li <terryliyixuan@gmail.com>
2016-10-09 13:22:51 -0400

    Edited the resolution and build settings.

---
+++ Assets/Scripts/SetResolution.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SetResolution : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start ()
+	{
+		Screen.SetResolution (1920, 1080, true);
+	}
+}
